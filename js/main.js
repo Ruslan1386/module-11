471fb183d5e8503689a16052979b429ec89a70a3
Ruslan <ruslan13_86@ukr.net>
2018-03-11 18:24:44 +0200

    chenge

--- js/main.js
+++ js/main.js
@@ -3,13 +3,10 @@ const tBody = document.querySelector("#js-tbody");
 const allUsers = document.querySelector("#all-user").textContent.trim();
 const buttonSearchById = document.querySelector("#js-search-by-ID");
 const buttonAddUser = document.querySelector("#js-add-user");
-const buttunDeleteUser = document.querySelector("#js-delete-by-ID")
+const buttunDeleteUser = document.querySelector("#js-delete-by-ID");
+const buttonUpdateUser = document.querySelector("#js-updata-by-ID");
 
 buttonGetUser.addEventListener("click", getAllUsers);
-buttonSearchById.addEventListener("click", getUserById);
-buttonAddUser.addEventListener("click", addUser);
-buttunDeleteUser.addEventListener("click", removeUser);
-
 const compile = _.template(allUsers);
 const updateView = user => {
 	let userString = "";
@@ -20,8 +17,8 @@ const updateView = user => {
 };
 
 //ф-я для получения всех пользователей БД
-function getAllUsers() {
-	event.preventDefault()
+function getAllUsers(event) {
+	event.preventDefault();
 	fetch("https://test-users-api.herokuapp.com/users/")
 	.then(response => {
 		if (response.ok) return response.json();
@@ -38,28 +35,32 @@ function getAllUsers() {
 };
 
 //ф-я для получения пользователя с id
-function getUserById(id) {
-	event.preventDefault()
+function getUserById(id) {	
 	id = document.querySelector("#input-ID");
-	const resultById = document.querySelector(".result-ID");
+	const resultById = document.querySelector(".result-ID");		
 	fetch("https://test-users-api.herokuapp.com/users/" + id.value)
 	.then(response => {
 		if (response.ok) return response.json();
 		throw new Error("Error fetching data");
 	})
 	.then(user => {
-	//console.log(user.data); 
+		console.log(user.data); 
 	//updateView(user.data); 
 	resultById.textContent = JSON.stringify(user.data.name);   
 })
 	.catch(error => {
 		console.error("Error: ", error);
-	});
+	});		
 };
 
+const getById = event => {
+	event.preventDefault();
+	getUserById(id);
+};
+buttonSearchById.addEventListener("click", getById);
+
 //ф-я для записи пользователя с полями name age
-function addUser(name, age) {
-	event.preventDefault()
+function addUser(name, age) {	
 	name = document.querySelector("#name");
 	age = document.querySelector("#age");	
 	fetch('https://test-users-api.herokuapp.com/users', {
@@ -69,29 +70,37 @@ function addUser(name, age) {
 			Accept: 'application/json',
 			'Content-Type': 'application/json',
 		}
-	});	
+	});			
 };
+const add = event => {
+	event.preventDefault();
+	addUser(name, age);
+};
+buttonAddUser.addEventListener("click", add);
 
 //ф-я для удаления пользователя с id
-function removeUser(id) {
-	event.preventDefault()
+function removeUser(id) {	
 	id = document.querySelector("#delete-user");
 	fetch('https://test-users-api.herokuapp.com/users' + id.value , {
 		method: 'delete',
 		headers: {
 			"X-Custom-Header": "",
 		}
-	});
+	});		
 };
+const remove = event => {
+	event.preventDefault();
+	removeUser(id);
+};
+buttunDeleteUser.addEventListener("click", remove);
 
 //функция для обновления данных по id.user 
-function updateUser(id, user) {
-	event.preventDefault()
-	id = document.querySelector("#updata-user");
-	user = {
+const user = {
 		name: "Ruuus",
 		age: 111111
 	};
+function updateUser(id, user) {	
+	id = document.querySelector("#updata-user");	
 	fetch('https://test-users-api.herokuapp.com/users', {
 		method: 'POST',
 		body: JSON.stringify({ name: user.name, age: user.age}),
@@ -99,5 +108,10 @@ function updateUser(id, user) {
 			Accept: 'application/json',
 			'Content-Type': 'application/json',
 		}
-	});
+	});		
 };
+const update = event => {
+	event.preventDefault();
+	updateUser(id, user);
+};
+buttonUpdateUser.addEventListener("click", update);
